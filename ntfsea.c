eed86bf75a06ece4580e7fea184e54e6f0f38bea
pdaxrom <sashz@pdaxrom.org>
2019-12-21 09:21:17 +0800

    surrogate links encoding fixed

--- ntfsea.c
+++ ntfsea.c
@@ -28,30 +28,20 @@
 
 #include "ntfsea.h"
 
-NTSYSAPI
-NTSTATUS
-NTAPI
-NtQueryEaFile (
-  /*IN*/ HANDLE               FileHandle,
-  /*OUT*/ PIO_STATUS_BLOCK    IoStatusBlock,
-  /*OUT*/ PVOID               Buffer,
-  /*IN*/ ULONG                Length,
-  /*IN*/ BOOLEAN              ReturnSingleEntry,
-  /*IN*/ PVOID                EaList /*OPTIONAL*/,
-  /*IN*/ ULONG                EaListLength,
-  /*IN*/ PULONG               EaIndex /*OPTIONAL*/,
-  /*IN*/ BOOLEAN              RestartScan
-);
+NTSYSAPI NTSTATUS NTAPI
+NtQueryEaFile( /*IN*/ HANDLE FileHandle,
+	      /*OUT*/ PIO_STATUS_BLOCK IoStatusBlock,
+	      /*OUT*/ PVOID Buffer,
+	       /*IN*/ ULONG Length,
+	       /*IN*/ BOOLEAN ReturnSingleEntry,
+	       /*IN*/ PVOID EaList /*OPTIONAL*/,
+	       /*IN*/ ULONG EaListLength, /*IN*/ PULONG EaIndex /*OPTIONAL*/, /*IN*/ BOOLEAN RestartScan);
 
-NTSYSAPI
-NTSTATUS
-NTAPI
-NtSetEaFile (
-  /*IN*/ HANDLE               FileHandle,
-  /*OUT*/ PIO_STATUS_BLOCK    IoStatusBlock,
-  /*OUT*/ PVOID               Buffer,
-  /*IN*/ ULONG                Length
-);
+NTSYSAPI NTSTATUS NTAPI
+NtSetEaFile( /*IN*/ HANDLE FileHandle,
+	    /*OUT*/ PIO_STATUS_BLOCK IoStatusBlock,
+	    /*OUT*/ PVOID Buffer,
+	     /*IN*/ ULONG Length);
 
 /*!
  * Opens the requested file for reading or writing.
@@ -65,30 +55,37 @@ NtSetEaFile (
  */
 HANDLE GetFileHandle(PWSTR DosFileName, BOOL Write, PFILE_FULL_EA_INFORMATION EaBuffer, ULONG EaLength)
 {
-	UNICODE_STRING FileName;
-	OBJECT_ATTRIBUTES ObjectAttributes;
-	ACCESS_MASK DesiredAccess = GENERIC_READ;
-	HANDLE FileHandle;
-	IO_STATUS_BLOCK IoStatusBlock;
+    UNICODE_STRING FileName;
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    ACCESS_MASK DesiredAccess = GENERIC_READ;
+    HANDLE FileHandle;
+    IO_STATUS_BLOCK IoStatusBlock;
 
-	if (Write)
-	{
-		DesiredAccess |= GENERIC_WRITE;
-	}
+    if (Write) {
+	DesiredAccess |= GENERIC_WRITE;
+    }
 
-	if (!RtlDosPathNameToNtPathName_U(DosFileName, &FileName, NULL, NULL))
-	{
-		return NULL;
-	}
+    if (!RtlDosPathNameToNtPathName_U(DosFileName, &FileName, NULL, NULL)) {
+	return NULL;
+    }
 
-	InitializeObjectAttributes(&ObjectAttributes, &FileName, 0, NULL, NULL);
+    InitializeObjectAttributes(&ObjectAttributes, &FileName, 0, NULL, NULL);
 
-	if (NtCreateFile(&FileHandle, DesiredAccess, &ObjectAttributes, &IoStatusBlock, NULL, 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, FILE_OPEN_IF, 0, EaBuffer, EaLength))
-	{
-		return NULL;
-	}
+    if (NtCreateFile(&FileHandle,
+		     DesiredAccess,
+		     &ObjectAttributes,
+		     &IoStatusBlock,
+		     NULL,
+		     0,
+		     FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+		     FILE_OPEN_IF,
+		     0,
+		     EaBuffer,
+		     EaLength)) {
+	return NULL;
+    }
 
-	return FileHandle;
+    return FileHandle;
 }
 
 /*!
@@ -98,57 +95,61 @@ HANDLE GetFileHandle(PWSTR DosFileName, BOOL Write, PFILE_FULL_EA_INFORMATION Ea
  *
  * \return List of extended attributes or NULL on error.
  */
-DLL_EXPORT struct EaList* GetEaList(PWSTR FileName)
+DLL_EXPORT struct EaList *GetEaList(PWSTR FileName)
 {
-	NTSTATUS Status = 0;
-	HANDLE FileHandle;
-	IO_STATUS_BLOCK IoStatusBlock = { 0 };
-	CHAR Buffer[MAX_LIST_LEN];
-	PFILE_FULL_EA_INFORMATION EaBuffer;
-	BOOLEAN RestartScan = TRUE;
-	struct EaList* Result = (struct EaList*)malloc(sizeof(struct EaList));
+    NTSTATUS Status = 0;
+    HANDLE FileHandle;
+    IO_STATUS_BLOCK IoStatusBlock = { 0 };
+    CHAR Buffer[MAX_LIST_LEN];
+    PFILE_FULL_EA_INFORMATION EaBuffer;
+    BOOLEAN RestartScan = TRUE;
+    struct EaList *Result = (struct EaList *)malloc(sizeof(struct EaList));
 
-	FileHandle = GetFileHandle(FileName, FALSE, NULL, 0);
-	if (FileHandle == NULL)
-	{
-		free(Result);
-		return NULL;
+    FileHandle = GetFileHandle(FileName, FALSE, NULL, 0);
+    if (FileHandle == NULL) {
+	free(Result);
+	return NULL;
+    }
+
+    do {
+	EaBuffer = (PFILE_FULL_EA_INFORMATION) Buffer;
+
+	Status = NtQueryEaFile(FileHandle,
+			       &IoStatusBlock,
+			       EaBuffer,
+			       MAX_LIST_LEN,
+			       FALSE,
+			       NULL,
+			       0,
+			       NULL,
+			       RestartScan);
+	if (Status != STATUS_SUCCESS && Status != STATUS_BUFFER_OVERFLOW) {
+	    NtClose(FileHandle);
+	    free(Result);
+	    return NULL;
 	}
 
-	do
-	{
-		EaBuffer = (PFILE_FULL_EA_INFORMATION)Buffer;
+	while (EaBuffer) {
+	    strcpy_s(Result->List[Result->ListSize].Name, MAX_EA_VALUE, EaBuffer->EaName);
+	    memcpy_s(Result->List[Result->ListSize].Value, MAX_EA_VALUE,
+		     EaBuffer->EaName + EaBuffer->EaNameLength + 1, EaBuffer->EaValueLength);
+	    Result->List[Result->ListSize].ValueLength = EaBuffer->EaValueLength;
+	    Result->ListSize++;
 
-		Status = NtQueryEaFile(FileHandle, &IoStatusBlock, EaBuffer, MAX_LIST_LEN, FALSE, NULL, 0, NULL, RestartScan);
-		if (Status != STATUS_SUCCESS && Status != STATUS_BUFFER_OVERFLOW)
-		{
-			NtClose(FileHandle);
-			free(Result);
-			return NULL;
-		}
+	    if (EaBuffer->NextEntryOffset == 0) {
+		break;
+	    }
 
-		while (EaBuffer)
-		{
-			strcpy_s(Result->List[Result->ListSize].Name, MAX_EA_VALUE, EaBuffer->EaName);
-			memcpy_s(Result->List[Result->ListSize].Value, MAX_EA_VALUE, EaBuffer->EaName + EaBuffer->EaNameLength + 1, EaBuffer->EaValueLength);
-			Result->List[Result->ListSize].ValueLength = EaBuffer->EaValueLength;
-			Result->ListSize++;
-
-			if (EaBuffer->NextEntryOffset == 0)
-			{
-				break;
-			}
-
-			EaBuffer = (PFILE_FULL_EA_INFORMATION)((PCHAR)EaBuffer + EaBuffer->NextEntryOffset);
-		}
-
-		RestartScan = FALSE;
+	    EaBuffer = (PFILE_FULL_EA_INFORMATION) ((PCHAR) EaBuffer + EaBuffer->NextEntryOffset);
 	}
-	while (Status == STATUS_BUFFER_OVERFLOW);
 
-	NtClose(FileHandle);
+	RestartScan = FALSE;
+    }
+    while (Status == STATUS_BUFFER_OVERFLOW);
 
-	return Result;
+    NtClose(FileHandle);
+
+    return Result;
 }
 
 /*!
@@ -159,58 +160,63 @@ DLL_EXPORT struct EaList* GetEaList(PWSTR FileName)
  *
  * \return Extended attribute information or NULL on error.
  */
-DLL_EXPORT struct Ea* GetEa(PWSTR FileName, PSTR EaName)
+DLL_EXPORT struct Ea *GetEa(PWSTR FileName, PSTR EaName)
 {
-	NTSTATUS Status = 0;
-	HANDLE FileHandle;
-	IO_STATUS_BLOCK IoStatusBlock = { 0 };
-	CHAR GetBuffer[MAX_LIST_LEN] = { 0 };
-	CHAR FullBuffer[MAX_LIST_LEN] = { 0 };
-	PFILE_GET_EA_INFORMATION EaList = (PFILE_GET_EA_INFORMATION)GetBuffer;
-	PFILE_GET_EA_INFORMATION EaQuery = EaList;
-	PFILE_FULL_EA_INFORMATION EaBuffer = (PFILE_FULL_EA_INFORMATION)FullBuffer;
-	ULONG EaListLength = 0;
-	ULONG EaNameLength = strlen(EaName);
-	struct Ea* Result = (struct Ea*)malloc(sizeof(struct Ea));
+    NTSTATUS Status = 0;
+    HANDLE FileHandle;
+    IO_STATUS_BLOCK IoStatusBlock = { 0 };
+    CHAR GetBuffer[MAX_LIST_LEN] = { 0 };
+    CHAR FullBuffer[MAX_LIST_LEN] = { 0 };
+    PFILE_GET_EA_INFORMATION EaList = (PFILE_GET_EA_INFORMATION) GetBuffer;
+    PFILE_GET_EA_INFORMATION EaQuery = EaList;
+    PFILE_FULL_EA_INFORMATION EaBuffer = (PFILE_FULL_EA_INFORMATION) FullBuffer;
+    ULONG EaListLength = 0;
+    ULONG EaNameLength = strlen(EaName);
+    struct Ea *Result = (struct Ea *)malloc(sizeof(struct Ea));
 
-	FileHandle = GetFileHandle(FileName, FALSE, NULL, 0);
-	if (FileHandle == NULL)
-	{
-		free(Result);
-		return NULL;
-	}
+    FileHandle = GetFileHandle(FileName, FALSE, NULL, 0);
+    if (FileHandle == NULL) {
+	free(Result);
+	return NULL;
+    }
 
-	EaNameLength = (ULONG)((EaNameLength + 1) * sizeof(CHAR));
-	memcpy_s(EaQuery->EaName, EaNameLength, EaName, EaNameLength);
-	EaQuery->EaNameLength = (UCHAR)EaNameLength - sizeof(CHAR);
+    EaNameLength = (ULONG) ((EaNameLength + 1) * sizeof(CHAR));
+    memcpy_s(EaQuery->EaName, EaNameLength, EaName, EaNameLength);
+    EaQuery->EaNameLength = (UCHAR) EaNameLength - sizeof(CHAR);
 
-	EaQuery->NextEntryOffset = FIELD_OFFSET(FILE_GET_EA_INFORMATION, EaName) + EaQuery->EaNameLength + sizeof(CHAR);
-	EaListLength += EaQuery->NextEntryOffset;
-	EaQuery->NextEntryOffset = 0;
+    EaQuery->NextEntryOffset = FIELD_OFFSET(FILE_GET_EA_INFORMATION, EaName) + EaQuery->EaNameLength + sizeof(CHAR);
+    EaListLength += EaQuery->NextEntryOffset;
+    EaQuery->NextEntryOffset = 0;
 
-	EaQuery = (PFILE_GET_EA_INFORMATION)((PCHAR)EaQuery + EaQuery->NextEntryOffset);
+    EaQuery = (PFILE_GET_EA_INFORMATION) ((PCHAR) EaQuery + EaQuery->NextEntryOffset);
 
-	Status = NtQueryEaFile(FileHandle, &IoStatusBlock, EaBuffer, MAX_FULLEA, FALSE, EaList, EaListLength, NULL, TRUE);
-	if (Status != STATUS_SUCCESS)
-	{
-		NtClose(FileHandle);
-		free(Result);
-		return NULL;
-	}
-
-	if (EaBuffer && EaBuffer->EaValueLength > 0)
-	{
-		strcpy_s(Result->Name, MAX_EA_VALUE, EaBuffer->EaName);
-		memcpy_s(Result->Value, MAX_EA_VALUE, EaBuffer->EaName + EaBuffer->EaNameLength + 1, EaBuffer->EaValueLength);
-		Result->ValueLength = EaBuffer->EaValueLength;
-	} else {
-		free(Result);
-		Result = NULL;
-	}
-
+    Status = NtQueryEaFile(FileHandle,
+			   &IoStatusBlock,
+			   EaBuffer,
+			   MAX_FULLEA,
+			   FALSE,
+			   EaList,
+			   EaListLength,
+			   NULL,
+			   TRUE);
+    if (Status != STATUS_SUCCESS) {
 	NtClose(FileHandle);
+	free(Result);
+	return NULL;
+    }
 
-	return Result;
+    if (EaBuffer && EaBuffer->EaValueLength > 0) {
+	strcpy_s(Result->Name, MAX_EA_VALUE, EaBuffer->EaName);
+	memcpy_s(Result->Value, MAX_EA_VALUE, EaBuffer->EaName + EaBuffer->EaNameLength + 1, EaBuffer->EaValueLength);
+	Result->ValueLength = EaBuffer->EaValueLength;
+    } else {
+	free(Result);
+	Result = NULL;
+    }
+
+    NtClose(FileHandle);
+
+    return Result;
 }
 
 /*!
@@ -225,58 +231,54 @@ DLL_EXPORT struct Ea* GetEa(PWSTR FileName, PSTR EaName)
  */
 DLL_EXPORT LONG32 WriteEa(PWSTR FileName, PSTR EaName, PSTR EaValue, ULONG32 EaValueLength)
 {
-	HANDLE FileHandle;
-	ULONG EaNameLength = strlen(EaName);
-	CHAR Buffer[MAX_FULLEA] = { 0 };
-	IO_STATUS_BLOCK IoStatusBlock = { 0 };
-	PFILE_FULL_EA_INFORMATION EaBuffer = NULL;
-	ULONG EaLength = 0;
+    HANDLE FileHandle;
+    ULONG EaNameLength = strlen(EaName);
+    CHAR Buffer[MAX_FULLEA] = { 0 };
+    IO_STATUS_BLOCK IoStatusBlock = { 0 };
+    PFILE_FULL_EA_INFORMATION EaBuffer = NULL;
+    ULONG EaLength = 0;
 
-	FileHandle = GetFileHandle(FileName, TRUE, EaBuffer, EaLength);
-	if (FileHandle == NULL)
-	{
-		return -1;
-	}
+    FileHandle = GetFileHandle(FileName, TRUE, EaBuffer, EaLength);
+    if (FileHandle == NULL) {
+	return -1;
+    }
 
-	EaBuffer = (PFILE_FULL_EA_INFORMATION)Buffer;
-	EaBuffer->NextEntryOffset = 0;
-	EaBuffer->Flags = 0;
+    EaBuffer = (PFILE_FULL_EA_INFORMATION) Buffer;
+    EaBuffer->NextEntryOffset = 0;
+    EaBuffer->Flags = 0;
 
-	EaNameLength = (ULONG)((EaNameLength + 1) * sizeof(CHAR));
-	memcpy_s(EaBuffer->EaName, EaNameLength, EaName, EaNameLength);
-	EaBuffer->EaNameLength = (UCHAR)EaNameLength - sizeof(CHAR);
+    EaNameLength = (ULONG) ((EaNameLength + 1) * sizeof(CHAR));
+    memcpy_s(EaBuffer->EaName, EaNameLength, EaName, EaNameLength);
+    EaBuffer->EaNameLength = (UCHAR) EaNameLength - sizeof(CHAR);
 
-	if (EaValue == NULL)
-	{
-		EaBuffer->EaValueLength = 0;
-	}
-	else
-	{
-		EaValueLength = (ULONG)((EaValueLength + 1) * sizeof(CHAR));
-		memcpy_s(EaBuffer->EaName + EaBuffer->EaNameLength + sizeof(CHAR), EaValueLength, EaValue, EaValueLength);
-		EaBuffer->EaValueLength = EaValueLength - sizeof(CHAR);
-	}
+    if (EaValue == NULL) {
+	EaBuffer->EaValueLength = 0;
+    } else {
+	EaValueLength = (ULONG) ((EaValueLength + 1) * sizeof(CHAR));
+	memcpy_s(EaBuffer->EaName + EaBuffer->EaNameLength + sizeof(CHAR), EaValueLength, EaValue, EaValueLength);
+	EaBuffer->EaValueLength = EaValueLength - sizeof(CHAR);
+    }
 
-	EaLength = FIELD_OFFSET(FILE_FULL_EA_INFORMATION, EaName) + EaBuffer->EaNameLength + sizeof(CHAR) + EaBuffer->EaValueLength;
+    EaLength = FIELD_OFFSET(FILE_FULL_EA_INFORMATION, EaName) + EaBuffer->EaNameLength +
+	       sizeof(CHAR) + EaBuffer->EaValueLength;
 
-	if (NtSetEaFile(FileHandle, &IoStatusBlock, EaBuffer, EaLength))
-	{
-		NtClose(FileHandle);
-		return -1;
-	}
-
+    if (NtSetEaFile(FileHandle, &IoStatusBlock, EaBuffer, EaLength)) {
 	NtClose(FileHandle);
+	return -1;
+    }
 
-	return EaBuffer->EaValueLength;
+    NtClose(FileHandle);
+
+    return EaBuffer->EaValueLength;
 }
 
-BOOL WSL_getMode(PWSTR FileName, mode_t *Mode)
+BOOL WSL_getMode(PWSTR FileName, mode_t * Mode)
 {
     struct Ea *ea = GetEa(FileName, "$LXMOD");
     if (!ea) {
 	return FALSE;
     }
-    uint32_t *ptr = (uint32_t *)ea->Value;
+    uint32_t *ptr = (uint32_t *) ea->Value;
     *Mode = *ptr;
     free(ea);
     return TRUE;
@@ -285,7 +287,7 @@ BOOL WSL_getMode(PWSTR FileName, mode_t *Mode)
 BOOL WSL_setMode(PWSTR FileName, mode_t Mode)
 {
     uint32_t val = Mode;
-    if (WriteEa(FileName, "$LXMOD", (PSTR)&val, 4) == -1) {
+    if (WriteEa(FileName, "$LXMOD", (PSTR) & val, 4) == -1) {
 	return FALSE;
     }
     return TRUE;
--- winlinks.c
+++ winlinks.c
@@ -1,83 +1,77 @@
+#ifdef WIN32
+#include "config.h"
+#include "daemon.h"
+#include "winsupport.h"
 #include <windows.h>
 #include <ntdef.h>
 #include <stdio.h>
 
 ssize_t ReadLinkW(const wchar_t * pathname, wchar_t * buf, size_t bufsiz)
 {
-	size_t path_len = -1;
-	REPARSE_DATA_BUFFER rep_data[REPARSE_DATA_BUFFER_HEADER_SIZE +
-				     MAXIMUM_REPARSE_DATA_BUFFER_SIZE];
-	DWORD get_size;
+    size_t path_len = -1;
+    REPARSE_DATA_BUFFER rep_data[REPARSE_DATA_BUFFER_HEADER_SIZE + MAXIMUM_REPARSE_DATA_BUFFER_SIZE];
+    DWORD get_size;
 
-	DWORD attr = GetFileAttributesW(pathname);
+    DWORD attr = GetFileAttributesW(pathname);
 
-	if ((attr == INVALID_FILE_ATTRIBUTES) ||
-	    !(attr & FILE_ATTRIBUTE_REPARSE_POINT)) {
-	    return -1;
-	}
+    if ((attr == INVALID_FILE_ATTRIBUTES) || !(attr & FILE_ATTRIBUTE_REPARSE_POINT)) {
+	return -1;
+    }
 
-	HANDLE hFile = CreateFileW(pathname,
-				   FILE_READ_EA,
-				   FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-				   NULL,
-				   OPEN_EXISTING,
-				   FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
-				   NULL);
+    HANDLE hFile = CreateFileW(pathname,
+			       FILE_READ_EA,
+			       FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+			       NULL,
+			       OPEN_EXISTING,
+			       FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+			       NULL);
 
-	if (hFile == INVALID_HANDLE_VALUE) {
-		printf("Could not open file (error %ld\n)", GetLastError());
-		goto out;
-	}
+    if (hFile == INVALID_HANDLE_VALUE) {
+	logmsg(LOG_ERR, "Could not open file (error %ld)", GetLastError());
+	goto out;
+    }
 
-	if (DeviceIoControl(hFile,
-			    FSCTL_GET_REPARSE_POINT,
-			    NULL,
-			    0,
-			    rep_data,
-			    sizeof(rep_data),
-			    &get_size,
-			    NULL) != 0) {
-		if (rep_data->ReparseTag == IO_REPARSE_TAG_SYMLINK) {
-			if (rep_data->SymbolicLinkReparseBuffer.Flags & 0x0001) {
-				printf("symbolic link relatived!\n");
-			}
-			path_len = rep_data->SymbolicLinkReparseBuffer.SubstituteNameLength;
-			path_len = (path_len > bufsiz) ? bufsiz : path_len;
-			memcpy(buf, &((BYTE *) rep_data->SymbolicLinkReparseBuffer.PathBuffer)
-			       [rep_data->SymbolicLinkReparseBuffer.SubstituteNameOffset],
-			       path_len);
-			//path[path_len / sizeof(wchar_t)] = 0;
-			//wprintf(L"--> %s\n", path);
-		} else if (rep_data->ReparseTag == IO_REPARSE_TAG_MOUNT_POINT) {
-			path_len = rep_data->MountPointReparseBuffer.SubstituteNameLength;
-			path_len = (path_len > bufsiz) ? bufsiz : path_len;
-			memcpy(buf, &((BYTE *) rep_data->MountPointReparseBuffer.PathBuffer)
-			       [rep_data->MountPointReparseBuffer.SubstituteNameOffset], path_len);
-		} else {
-			printf("unsupported reparse tag %08X\n", rep_data->ReparseTag);
-			if (IsReparseTagNameSurrogate(rep_data->ReparseTag)) {
-				path_len = rep_data->ReparseDataLength - 4;
-				path_len = (path_len > bufsiz) ? bufsiz : path_len;
-				printf("surrogate (%d)\n", rep_data->ReparseDataLength);
-				memcpy(buf,
-				       &((BYTE *) rep_data->GenericReparseBuffer.DataBuffer)[4],
-				       path_len);
-				//path[path_len / sizeof(wchar_t)] = 0;
-//                      wprintf(L"--> %s\n", path);
-//                      printf("--> %s\n", path);
-			}
-		}
+    if (DeviceIoControl(hFile, FSCTL_GET_REPARSE_POINT, NULL, 0, rep_data, sizeof(rep_data), &get_size, NULL) != 0) {
+	if (rep_data->ReparseTag == IO_REPARSE_TAG_SYMLINK) {
+	    if (rep_data->SymbolicLinkReparseBuffer.Flags & 0x0001) {
+		logmsg(LOG_INFO, "symbolic link relatived!");
+	    }
+	    path_len = rep_data->SymbolicLinkReparseBuffer.SubstituteNameLength;
+	    path_len = (path_len > bufsiz) ? bufsiz : path_len;
+	    memcpy(buf, &((BYTE *) rep_data->SymbolicLinkReparseBuffer.PathBuffer)
+		   [rep_data->SymbolicLinkReparseBuffer.SubstituteNameOffset], path_len);
+	} else if (rep_data->ReparseTag == IO_REPARSE_TAG_MOUNT_POINT) {
+	    path_len = rep_data->MountPointReparseBuffer.SubstituteNameLength;
+	    path_len = (path_len > bufsiz) ? bufsiz : path_len;
+	    memcpy(buf, &((BYTE *) rep_data->MountPointReparseBuffer.PathBuffer)
+		   [rep_data->MountPointReparseBuffer.SubstituteNameOffset], path_len);
 	} else {
-		printf("can't read reparse data %ld\n", GetLastError());
+	    logmsg(LOG_WARNING, "unsupported reparse tag %08X", (unsigned int)rep_data->ReparseTag);
+	    if (IsReparseTagNameSurrogate(rep_data->ReparseTag) && rep_data->ReparseTag == 0xA000001D) {
+		logmsg(LOG_INFO, "surrogate (%d)", rep_data->ReparseDataLength);
+		path_len = MultiByteToWideChar(CP_UTF8,
+					       0,
+					       &((LPCCH) rep_data->GenericReparseBuffer.DataBuffer)[4],
+					       rep_data->ReparseDataLength - 4,
+					       buf,
+					       bufsiz);
+		path_len *= sizeof(wchar_t);
+		if (!path_len) {
+		    logmsg(LOG_CRIT, "%s: MultiByteToWideChar failed", __func__);
+		}
+	    }
 	}
+    } else {
+	logmsg(LOG_ERR, "can't read reparse data %ld", GetLastError());
+    }
 
  out:
-	CloseHandle(hFile);
+    CloseHandle(hFile);
 
-	return path_len;
+    return path_len;
 }
 
-int SymLinkW(const wchar_t *target, const wchar_t *linkpath)
+int SymLinkW(const wchar_t * target, const wchar_t * linkpath)
 {
     return -1;
 }
@@ -86,7 +80,7 @@ int SymLinkW(const wchar_t *target, const wchar_t *linkpath)
 int main(int argc, char *argv[])
 {
     const size_t WCHARBUF = 100;
-    wchar_t  pathname[WCHARBUF];
+    wchar_t pathname[WCHARBUF];
     MultiByteToWideChar(CP_ACP, MB_PRECOMPOSED, argv[1], -1, pathname, WCHARBUF);
 
     if (argc != 2) {
@@ -110,17 +104,18 @@ int main(int argc, char *argv[])
 }
 #endif
 
-#if 0
-	DWORD attr = GetFileAttributes(argv[1]);
-	if (attr == INVALID_FILE_ATTRIBUTES) {
-		if (GetLastError() == ERROR_FILE_NOT_FOUND) {
-			printf("File not found\n");
-			return 1;
-		}
-		printf("Error!\n");
-	}
+/*
+DWORD attr = GetFileAttributes(argv[1]);
+if (attr == INVALID_FILE_ATTRIBUTES) {
+    if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+	printf("File not found\n");
+	return 1;
+    }
+    printf("Error!\n");
+}
 
-	if (attr & FILE_ATTRIBUTE_REPARSE_POINT) {
-		printf("symlink\n");
-	}
-#endif
+if (attr & FILE_ATTRIBUTE_REPARSE_POINT) {
+    printf("symlink\n");
+}
+ */
+#endif /* WIN32 */
